33ebed3fd24f1a3c25b381836c3d6dac16f98791
jesseck3013 <jesseck3013@outlook.com>
2024-12-03 22:47:19 +0800

    added a script

---
+++ New-Dotnet-Project.ps1
@@ -0,0 +1,23 @@
+function New-Dotnet-Project {
+    param (
+	[validateset("web", "console", "classlib")]
+        [Parameter(Mandatory)]
+	[string]$Template,
+	[Parameter(Mandatory)]
+	[string]$ProjectName
+    )
+
+    $SlnDir = "${ProjectName}-test"
+    $ProjectDir = "${ProjectName}"
+    $TestsDir = "${ProjectName}.Tests"
+
+    dotnet new sln -o $SlnDir
+    cd $SlnDir
+    dotnet new $Template -o $ProjectName
+    dotnet sln add "./${ProjectName}/${ProjectName}.csproj"
+    dotnet new xunit -o $TestsDir
+    dotnet add "./${ProjectName}.Tests/${ProjectName}.Tests.csproj" reference "./${ProjectName}/${ProjectName}.csproj"
+    dotnet sln add "./${ProjectName}.Tests/${ProjectName}.Tests.csproj"
+}
+
+
